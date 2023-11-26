38156987358b317f32c46d7223e5f711780a063b
Edoardo Cipriano <edoardo.cipriano@polimi.it>
2023-11-26 13:54:45 +0100

    Fixed bug with old optional inputs

--- src/icentripetal.h
+++ src/icentripetal.h
@@ -185,7 +185,7 @@ void position_centripetal (scalar f, scalar pos, bool add = false)
       rho2vh = rho2v[];
 #endif
 
-      if (p.add)
+      if (add)
         pos[] += 1./hp*sfm.eps*(rho2vh - rho1vh);
       else
         pos[] = 1./hp*sfm.eps*(rho2vh - rho1vh);
