95b0816e7447b33c1db4b977b97e57890f75ea68
Edoardo Cipriano <edoardo.cipriano@polimi.it>
2025-06-19 19:29:58 +0200

    Added burningdroplet file

---
+++ run_dev/burningdroplet.c
@@ -0,0 +1,316 @@
+/**
+# Combustion of an axi droplet in microgravity or in normal gravity conditions
+
+This simulation file helps setting up numerical simulations of droplet
+combustion in axial symmetry, in any gravity conditions, and considering
+different kinetic schemes and chemical species.
+*/
+
+#include "axi.h"
+#include "navier-stokes/low-mach.h"
+#define FILTERED 1
+#define P_ERR 0.1
+#include "two-phase-varprop.h"
+#include "opensmoke/properties.h"
+#include "opensmoke/chemistry.h"
+//#include "pinning.h"
+#if VELOCITY_JUMP
+# include "two-phase-clsvof.h"
+#else
+# include "two-phase.h"
+#endif
+#include "tension.h"
+#include "gravity.h"
+#include "evaporation.h"
+#include "spark.h"
+#include "view.h"
+
+/**
+## Simulation setups
+
+We define an enum with different simulation setups: in microgravity and in
+normal gravity conditions. We use this approach because the first configuration
+place the droplet on the lower-left corner of the domain, in order to exploit
+the axis of symmetry and to avoid non-physical lateral displacement. In normal
+gravity, instead, we place the droplet along the edge of the bottom boundary,
+and we include the droplet suspension strategy. This enum helps managing
+differences among the two configurations.
+*/
+
+enum Setups { microgravity, gravity };
+enum Setups setup = microgravity;
+
+/**
+## Input data
+
+We declare a structure containing the input parameters for the simulations. We
+are resolving not only the fluid dynamics but also the temperature field and
+many chemical species (we don't know how many a-priori). Additionally, we need
+to include a kinetic scheme, and the rules for calculating the material
+properties in both the liquid and the gas phases. Since too many parameter have
+to be provided, we gather them in a structure of input data and we read all the
+parameters from an input file.
+*/
+
+struct {
+  char * kinetics_folder;
+  char * liquid_properties_folder;
+  int maxlevel;
+  double TL0;
+  double TG0;
+  double P0;
+  double D0;
+  double G;
+  double sigma;
+  double rfiber_to_rdrop;
+  double rgas_to_rdrop;
+  bool combustion;
+  bool fick_corrected;
+  bool molar_diffusion;
+  bool mass_diffusion_enthalpy;
+  bool divergence;
+  char * liq_start;
+  char * gas_start;
+  double spark_diameter;
+  double spark_start;
+  double spark_time;
+  double spark_value;
+} inputdata = {
+  "two-step/methanol",
+  "LiquidProperties",
+  10,       // maxlevel
+  300,      // TL0
+  300,      // TG0
+  101325,   // P0
+  1e-3,     // D0
+  0.,       // G
+  0.03,     // sigma
+  0.1,      // rfiber_to_rdrop;
+  80,       // rgas_to_rdrop;
+  true,     // divergence
+  true,     // combustion
+  true,     // fick_corrected
+  true,     // molar_diffusion
+  true,     // mass_diffusion_enthalpy
+  "CH3OH 1",            // liq_start
+  "N2 0.79 O2 0.21",    // gas_start
+  0.2,      // spark_diameter;
+  0.0,      // spark_start;
+  0.01,     // spark_time;
+  1e7,      // spark_value;
+};
+
+/**
+## Useful variables
+
+We declare other useful variabiles for this simulations.
+*/
+
+int maxlevel, minlevel = 2;
+double D0, R0, R, M0, DD02 = 1., tad = 0.;
+bool restored = false;
+scalar qspark[];
+
+int main (int argc, char ** argv) {
+
+  /**
+  We set the kinetics folders, for the gas and for the liquid kinetics. The
+  liquid properties are initialized as well. */
+
+  kinetics (inputdata.kinetics_folder, &NGS);
+  kinetics_liquid (inputdata.kinetics_folder, &NLS);
+  properties_liquid (inputdata.liquid_properties_folder);
+
+  /**
+  We specify the species names, in order to facilitate operations on the species
+  when lage kinetic schemes are used (in this context large means more than 4
+  species). */
+
+  gas_species = new_species_names (NGS);
+  liq_species = new_species_names_liquid (NLS);
+
+  /**
+  We set default simulation properties which will be overwritten by the variable
+  material properties formulations. We can skip this part but WE NEED TO DEFINE
+  NON-NULL VISCOSITY, because the [two-phase-generic.h](src/two-phase-generic.h)
+  module creates the variable viscosity field only if mu is non-zero. */
+
+  mu1 = mu2 = 1.;
+
+  /**
+  We set the initial thermodynamic state of the two-phase system (SI units). */
+
+  Pref = inputdata.P0;
+  TG0 = inputdata.TG0;
+  TL0 = inputdata.TL0;
+
+  /**
+  We use a two-velocities model, and we set additional options for the phase
+  change model `pcm`. */
+
+  nv = 2;
+  pcm.divergence = inputdata.divergence;
+  pcm.chemistry = inputdata.combustion;
+  pcm.fick_corrected = inputdata.fick_corrected;
+  pcm.molar_diffusion = inputdata.molar_diffusion;
+  pcm.mass_diffusion_enthalpy = inputdata.mass_diffusion_enthalpy;
+
+  /**
+  We change the dimensions of the domain according with the simulation setup
+  used. */
+
+  double RR = inputdata.rgas_to_rdrop;
+  D0 = inputdata.D0;
+  L0 = 0.5*RR*D0;
+  maxlevel = inputdata.maxlevel;
+
+  /**
+  Depending on the gravity value, we change the simulation setup. */
+
+  G.x = inputdata.G;
+  setup = G.x ? gravity : microgravity;
+
+  if (setup == gravity) {
+    double df = inputdata.rfiber_to_rdrop*D0;
+    X0 = -0.5*L0, Y0 = 0.5*df;
+
+    //pinning.ap = sqrt (sq (0.5*D0) - sq (Y0));
+    //pinning.ac = pinning.ap - 2.*L0/(1 << maxlevel);
+  }
+
+  /**
+  We set the surface tension and additional tracers if needed. */
+
+  f.sigma = inputdata.sigma;
+
+  /**
+  Setting up the grid and the pinning point if the setup requires droplet
+  suspension. Finally, we run the simulation. */
+
+  //init_grid (1 << min (maxlevel, 9));
+  init_grid (1 << maxlevel);
+  run();
+
+  /**
+  We de-allocate memory used for the vectors of species names. */
+
+  free_species_names (NGS, gas_species), gas_species = NULL;
+  free_species_names (NLS, liq_species), liq_species = NULL;
+}
+
+macro number circle (double x, double y, double R,
+    double x0 = 0., double y0 = 0.)
+{
+  return (sq(R) - sq(x - x0) - sq(y - y0));
+}
+
+event init (i = 0) {
+  if (!restore (file = "restart")) {
+#if TREE
+    refine (circle (x, y, 4.*D0) > 0. && level < maxlevel);
+#endif
+    fraction (f, circle (x, y, 0.5*D0));
+
+    /**
+    We compute initial variables, useful for post-processing. */
+
+    R = R0 = pow(3./4./pi*(2.*pi*statsf(f).sum), 1./3.);
+
+    scalar rhol = liq->rho;
+    M0 = 0.;
+    foreach (reduction(+:M0))
+      M0 += rhol[]*f[]*dv();
+  }
+
+  /**
+  We set the initial thermodynamic state of the phases, and we overwrite some of
+  their properties. The function `phase_set_thermo_state` sets the initial
+  conditions of temperature, pressure, and mass fraction fields only if they are
+  uniform. Therefore, if `restore` was called, the function should not overwrite
+  the fields. However, it is still important to call it in order to store the
+  initial thermo state */
+
+  ThermoState tsl, tsg;
+  tsl.T = TL0, tsl.P = Pref, tsl.x = NULL;
+  tsg.T = TG0, tsg.P = Pref, tsg.x = NULL;
+
+  phase_set_thermo_state (liq, &tsl);
+  phase_set_thermo_state (gas, &tsg);
+
+  phase_set_composition_from_string (liq, inputdata.liq_start);
+  phase_set_composition_from_string (gas, inputdata.gas_start);
+
+  /**
+  The only property that we need to set, and that remains constant throughout
+  the simulation, is the vector with the molecular weight of each species. */
+
+  double MWLs[NLS], MWGs[NGS];
+  molecular_weights (NGS, MWGs);
+  foreach_species_in (liq)
+    MWLs[i] = MWGs[LSI[i]];
+
+  phase_set_properties (liq, MWs = MWLs);
+  phase_set_properties (gas, MWs = MWGs);
+
+#if TWO_PHASE_VARPROP
+  antoine = &opensmoke_antoine;
+#endif
+
+  /**
+  We set the spark properties. */
+
+#if SPARK
+  spark.T = qspark;
+  spark.position = (coord){0.75*D0, 0.75*D0};
+  spark.diameter = inputdata.spark_diameter*D0;
+  spark.time = inputdata.spark_start;
+  spark.duration = inputdata.spark_time;
+  spark.temperature = inputdata.spark_value;
+  spark.phase = gas;
+#endif
+}
+
+/**
+## Boundary conditions
+
+We set the boundary conditions in the `defaults` event because, in
+[navier-stokes/centered-new.h](navier-stokes/centered-new.h) the boundary
+conditions for the additional velocity fields are copied from u and p.
+*/
+
+event defaults (i = 0) {
+  u.n[top] = neumann (0.);
+  u.t[top] = neumann (0.);
+  p[top] = dirichlet (0.);
+
+  u.n[right] = neumann (0.);
+  u.t[right] = neumann (0.);
+  p[right] = dirichlet (0.);
+
+  if (setup == gravity) {
+    u.n[left] = neumann (0.);
+    u.t[left] = neumann (0.);
+    p[left] = dirichlet (0.);
+
+    u.n[bottom] = dirichlet (0.);
+    u.t[bottom] = dirichlet (0.);
+    p[bottom] = neumann (0.);
+    uf.n[bottom] = 0.;
+    uf.t[bottom] = 0.;
+  }
+}
+
+/**
+## Mesh adaptation
+
+We adapt the grid according to the fuel mass fraction, the temperature, and the
+velocity field. */
+
+#if TREE
+event adapt (i++) {
+  adapt_wavelet_leave_interface ({Y,T,u.x,u.y}, {f},
+      (double[]){1.e-1,1.e-0,1e-1,1e-1}, maxlevel, minlevel, 1);
+}
+#endif
+
+event stop (t = 50);
