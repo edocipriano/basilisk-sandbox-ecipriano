8e393e1dd9e34da23bacabdb0bc93e2b6d410db2
Edoardo Cipriano <edoardo.cipriano@polimi.it>
2025-09-04 16:39:00 +0200

    Improved description and added reference file for c7pathak

--- run_dev/c7pathak.c
+++ run_dev/c7pathak.c
@@ -1,42 +1,31 @@
 /**
 # Non-Isothermal Evaporation of a n-Heptane Droplet
 
-In this test case we want to simulate the evaporation of a
-n-heptane droplet in nitrogen. The droplet is pure in a non
-isothermal environment. Therefore, we want to solve both
-the chemical species mass fractions and the temperature
-field. At the beginning of the simulation, the droplet
-is initialized at 363K, while the ambient temperature is 565K.
-The heat conduction from the environment heats up the liquid
-droplet increasing the vapor pressure value and, therefore,
-increasing the evaporation rate of the droplet. The
-interface temperature tends to a plateau, given by the
-interplay between the heat conduction from the environment
-and the evaporation process that cools down the interface.
+In this test case we want to simulate the evaporation of a n-heptane droplet in
+nitrogen. The droplet is pure in a non isothermal environment. Therefore, we
+want to solve both the chemical species mass fractions and the temperature
+field. At the beginning of the simulation, the droplet is initialized at 363K,
+while the ambient temperature is 565K.  The heat conduction from the environment
+heats up the liquid droplet increasing the vapor pressure value and, therefore,
+increasing the evaporation rate of the droplet. The interface temperature tends
+to a plateau, given by the interplay between the heat conduction from the
+environment and the evaporation process that cools down the interface.
 
 ![Evolution of the temperature field (left) and the n-heptane mass fraction (right)](c7pathak/movie.mp4)(height=400 width=900)
 */
 
 /**
-## Phase Change Setup
-
-We define the number of gas and liquid species in the domain,
-we filter the density field to reduce problems related with
-the strong density ratio. The interface temperature must be
-obtained from the root-finding procedure with the *fsolve()*
-function, using the GSL interface. The Antoine equation is
-used according to the thermodynamic equilibrium implemented
-in [Pathak et al., 2018](#pathak2018steady), which proposed
-this test case. */
-
-/**
 ## Simulation Setup
 
-We use the centered solver with the evaporation source term
-in the projection step. The extended velocity is obtained
-from the doubled pressure-velocity coupling. We use the
-evaporation model together with the multiomponent phase
-change mechanism. */
+We use the centered Navier--Stokes equations solver with volumetric source in
+the projection step. The phase change is directly included using the evaporation
+module, which sets the best (default) configuration for evaporation problems.
+Many features of the phase change (evaporation) model can be modified directly
+in this file without changing the source code, using the phase change model
+object `pcm`. Compiling with `-DJUMP=1` changes the Navier--Stokes solver to the
+velocity-jump formulation, which employs a GFM approach to set the interface
+velocity jump. The density field is filtered in order to reduce convergence
+issues related with strong density ratios. */
 
 #include "axi.h"
 #if JUMP
@@ -45,11 +34,7 @@ change mechanism. */
 # include "navier-stokes/low-mach.h"
 #endif
 #define FILTERED 1
-#if VELOCITY_JUMP
-# include "two-phase-clsvof.h"
-#else
-# include "two-phase.h"
-#endif
+#include "two-phase.h"
 #include "tension.h"
 #include "evaporation.h"
 #include "view.h"
@@ -57,8 +42,9 @@ change mechanism. */
 /**
 ### Boundary conditions
 
-Outflow boundary conditions are set at the top and right
-sides of the domain. */
+Outflow boundary conditions are set at the top and right sides of the domain.
+Boundary conditions for species and temperature must be set in the `init` event
+since those fields are created in `defaults`. */
 
 u.n[top] = neumann (0.);
 u.t[top] = neumann (0.);
@@ -71,9 +57,8 @@ p[right] = dirichlet (0.);
 /**
 ### Simulation Data
 
-We declare the maximum and minimum levels of refinement,
-the initial radius and diameter, and the radius from the
-numerical simulation. */
+We declare the maximum and minimum levels of refinement, the initial radius and
+diameter, and the radius from the numerical simulation. */
 
 int maxlevel, minlevel = 2;
 double D0 = 5.e-6, effective_radius0 = 1.;
@@ -82,12 +67,21 @@ bool restored = false;
 
 int main (void) {
 
+  /**
+  The number of gas and of liquid species are set in the `main()` function. */
+
   NGS = 2, NLS = 1;
 
   /**
-  We set the material properties of the fluids. The
-  properties correspond to the n-heptane/nitrogen system
-  at 28.6 bar. */
+  We set the material properties of the two fluids. In addition to the classic
+  Basilisk setup for density and viscosity, we need to define the thermal and
+  species properties, such as the thermal conductivity $\lambda$, the heat
+  capacity $cp$, the enthalpy of vaporization $\Delta h_{ev}$, the diffusivity
+  $D$, and the thermodynamic pressure, which is assumed constant in space and
+  time. The species properties would require a vector with dimension equal to
+  the number of species. To facilitate this setup we first set those properties
+  which are common to all the species, and then we can refine the setup in the
+  `init` event passing vectors to the `phase_set_properties()` function. */
 
   rho1 = 626.7; rho2 = 17.51;
   mu1 = 1.e-3; mu2 = 1.e-5;
@@ -97,25 +91,30 @@ int main (void) {
   dhev = 3.23e5;
   Pref = 2860000.;
 
+  /**
+  We set the initial liquid and gas phase temperatures. */
+
   TL0 = 363., TG0 = 563.;
+
+  /**
+  We solve two different sets of Navier--Stokes equations according with the
+  double pressure velocity coupling approach. */
 
   nv = 2;
 
   /**
-  We change the dimension of the domain as a function
-  of the initial diameter of the droplet. */
+  We change the dimension of the domain as a function of the initial diameter of
+  the droplet. */
 
   L0 = 2.*D0;
 
   /**
-  We change the surface tension coefficient. and we
-  decrease the tolerance of the Poisson solver. */
+  We assume a constant surface tension coefficient. */
 
   f.sigma = 0.01;
 
   /**
-  We run the simulation at different maximum
-  levels of refinement. */
+  We run the simulation at different maximum levels of refinement. */
 
   for (maxlevel = 6; maxlevel <= 6; maxlevel++) {
     init_grid (1 << maxlevel);
@@ -126,30 +125,24 @@ int main (void) {
 #define circle(x,y,R)(sq(R) - sq(x) - sq(y))
 
 /**
-We initialize the volume fraction field and we compute the
-initial radius of the droplet. We don't use the value D0
-because for small errors of initialization the squared
-diameter decay would not start from 1. */
+We initialize the volume fraction field and we compute the initial radius of the
+droplet. We don't use the value D0 because for small errors of initialization
+the squared diameter decay would not start from 1. */
 
 event init (i = 0) {
   if (!restore (file = "restart")) {
     fraction (f, circle (x, y, 0.5*D0));
-#if VELOCITY_JUMP
-    foreach()
-      d[] = circle (x, y, 0.5*D0);
-    vertex scalar phi[];
-    foreach_vertex()
-      phi[] = (d[] + d[-1] + d[0,-1] + d[-1,-1])/4.;
-    fractions (phi, f);
-#endif
     effective_radius0 = pow(3.*statsf(f).sum, 1./3.);
   }
   else
     restored = true;
 
   /**
-  We set the molecular weights of the chemial species
-  involved in the simulation (by default inMW=1). */
+  The initial thermodynamics states of the two phases (i.e. the temperature,
+  pressure, and composition) are defined and set. We force setting the thermo
+  state only if the simulation was not restored. Finally, we introduce missing
+  properties which are different for each species, such as the molecular
+  weights. */
 
   ThermoState tsl, tsg;
   tsl.T = TL0, tsl.P = Pref, tsl.x = (double[]){1.};
@@ -162,9 +155,9 @@ event init (i = 0) {
   phase_set_properties (gas, MWs = (double[]){100.2,29.});
 
   /**
-  The proper Antoine equation function must be set
-  to the attribute *antoine* of the liquid phase mass
-  fraction fields. */
+  The proper Antoine equation function must be set to the attribute *antoine* of
+  the liquid phase mass fraction fields. In absence of this function, the value
+  `YIntVal` or the array `YIntVals` are used for the thermodynamic VLE. */
 
   scalar lfuel = liq->YList[0];
   lfuel.antoine = antoine_heptane;
@@ -206,9 +199,8 @@ event init (i = 0) {
 }
 
 /**
-We adapt the grid according to the mass fractions of the
-mass fraction of n-heptane, the temperature, and the
-velocity field. */
+We adapt the grid according to the mass fraction of the evaporating species, the
+temperature, and the velocity field. */
 
 #if TREE
 event adapt (i++) {
@@ -225,8 +217,7 @@ The following lines of code are for post-processing purposes. */
 /**
 ### Output Files
 
-We write on a file the squared diameter decay and the
-dimensionless time. */
+We write on a file the squared diameter decay and the dimensionless time. */
 
 event output_data (i++) {
   double effective_radius = pow(3.*statsf(f).sum, 1./3.);
@@ -243,10 +234,19 @@ event output_data (i++) {
 }
 
 /**
+### Logger
+
+We output the total liquid volume in time (for testing). */
+
+event logger (t += 1e-5) {
+  fprintf (stderr, "%d %.1g %.3g\n", i, t, statsf (f).sum);
+}
+
+/**
 ### Temperature and Mass Fraction Profiles
 
-We write on a file the temperature and mass fraction
-profiles at different time instants. */
+We write on a file the temperature and mass fraction profiles at different time
+instants. */
 
 event profiles (t = {3.29e-6, 3.e-5, 1.05e-4, 1.5e-4}) {
   for (double x = 0.; x < L0; x += 0.5*L0/(1 << maxlevel)) {
@@ -260,9 +260,8 @@ event profiles (t = {3.29e-6, 3.e-5, 1.05e-4, 1.5e-4}) {
 /**
 ### Movie
 
-We write the animation with the evolution of the
-n-heptane mass fraction, the interface position
-and the temperature field. */
+We write the animation with the evolution of the n-heptane mass fraction, the
+interface position and the temperature field. */
 
 event movie (t += 2.e-6; t <= 1.6e-4) {
   clear();
